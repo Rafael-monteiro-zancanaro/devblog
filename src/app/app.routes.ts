b7c3da58ef0bbe9321e45b9f95e7de25dbb59306
Rafael-monteiro-zancanaro <rafaelmonte46@gmail.com>
2026-03-14 16:40:44 -0300

    [Feat] Pagina de setup finalizada

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { ProjectsComponent } from './pages/projects/projects.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { SetupComponent } from './pages/setup/setup.component';
 
 export const routes: Routes = [
     {
@@ -16,6 +17,10 @@ export const routes: Routes = [
     {
         path: 'projects',
         component: ProjectsComponent
+    },
+    {
+        path: 'setup',
+        component: SetupComponent
     },
     {
         path: '**',
